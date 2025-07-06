4f6c3b2526517c3143935232c558ff6d260e63c6
Sujay Namburi <nambjay@Mac.attlocal.net>
2025-07-06 15:50:19 -0500

    Improve GitHub Actions workflow and remove docs folder for root deployment

--- docs/_next/static/chunks/app/layout-19a10c1f3ba67e17.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{7134:function(n,e,t){Promise.resolve().then(t.t.bind(t,9456,23)),Promise.resolve().then(t.t.bind(t,2778,23))},2778:function(){}},function(n){n.O(0,[507,216,971,744],function(){return n(n.s=7134)}),_N_E=n.O()}]);
